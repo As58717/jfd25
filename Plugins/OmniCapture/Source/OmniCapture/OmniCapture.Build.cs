7ec1a8c026e577680f4234667f556bb359db0c68
As58717 <i52de1fwc@hotmail.com>
2025-11-08 22:03:34 +0800

    Add D3D12 fallback for NVENC capability probe

--- Plugins/OmniCapture/Source/OmniCapture/OmniCapture.Build.cs
+++ Plugins/OmniCapture/Source/OmniCapture/OmniCapture.Build.cs
@@ -158,6 +158,8 @@ public class OmniCapture : ModuleRules
             }
 
             PublicAdditionalLibraries.Add("d3d11.lib");
+            PublicAdditionalLibraries.Add("d3d12.lib");
+            PublicAdditionalLibraries.Add("dxgi.lib");
 
             PrivateDefinitions.Add("WITH_OMNI_NVENC=1");
 
